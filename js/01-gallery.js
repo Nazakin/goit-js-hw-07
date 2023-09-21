5e2ec35b9dd76bdd0c1c4fc921032e2f64e6175f
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-09-22 00:03:30 +0200

    rst

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryElement = document.querySelector('.gallery');
+const gallery = galleryItems.map((image) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>
+`)
+galleryElement.innerHTML = gallery.join('');
 
 console.log(galleryItems);
