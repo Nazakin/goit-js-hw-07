9832122d48c188a932d3d817e618d2ff87550959
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-09-26 01:37:17 +0200

    фыфы

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,20 +1,24 @@
+import simpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryElement = document.querySelector('.gallery');
 const gallery = galleryItems.map((image) => `
 <li class="gallery__item">
-  <a class="gallery__link" href="${image.original}">
-    <img
-      class="gallery__image"
-      src="${image.preview}"
-      data-source="${image.original}"
-      alt="${image.description}"
-    />
-  </a>
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.preview}" />
+   </a>
 </li>
 `);
 galleryElement.innerHTML = gallery.join('');
+galleryElement.addEventListener("click", (event) => {
+  if (event.target.nodeName !== "A") {
+    event.preventDefault();
+    return;
+  }
+  const selectedImage = event.target.dataset.source;
+  const lightbox = new SimpleLightbox(selectedImage);
+  lightbox.open();
+}
 
 
-
-console.log(galleryItems);
+});
